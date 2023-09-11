ad1f409c21e1977cef7601c327ae3a95a0658cda
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2023-09-11 19:14:08 +0530

    internal-css-state-management

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
-import { useState } from "react";
 import "./app.css";
+import React, { useState } from "react";
+
+const styles = {
+  dark: {
+    color: "white",
+    backgroundColor: "black",
+  },
+  light: {
+    color: "black",
+    backgroundColor: "white",
+  },
+};
 
 function App() {
-  const [objData, setObjData] = useState({});
+  const [theme, setTheme] = useState(false);
 
-  const getInputs = (value, name) => {
-    let data = { [name]: value };
-
-    setObjData({ ...objData, ...data });
+  const changeTheme = () => {
+    setTheme(!theme);
   };
-
-  const submit = (e) => {
-    e.preventDefault();
-    console.log(objData);
-  };
-
   return (
-    <div className="container-fluid">
-      <form className="d-flex flex-column gap-4 w-25 bg-white p-3 mt-3 mx-auto">
-        <input
-          placeholder="Write your Name"
-          type="text"
-          name="name"
-          onChange={(e) => getInputs(e.target.value, e.target.name)}
-        />
-        <input
-          placeholder="Write your Age"
-          type="number"
-          name="age"
-          onChange={(e) => getInputs(e.target.value, e.target.name)}
-        />
-        <input
-          placeholder="Write your Hobby"
-          type="text"
-          name="hobbies"
-          onChange={(e) => getInputs(e.target.value, e.target.name)}
-        />
-        <input
-          type="date"
-          name="date"
-          onChange={(e) => getInputs(e.target.value, e.target.name)}
-        />
-        <button onClick={submit}>Submit</button>
-      </form>
+    <div className="container-fluid" style={theme ? styles.dark : styles.light}>
+      <h1>THEME</h1>
+      <button
+        className={
+          theme ? "btn btn-outline-light fs-2" : "btn btn-outline-dark fs-2"
+        }
+        onClick={changeTheme}
+      >
+        Toggle Theme
+      </button>
     </div>
   );
 }
